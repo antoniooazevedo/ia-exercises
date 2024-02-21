eb79719dc02d72ab8d831ac6b825b1d07663b398
antazevedo2003@gmail.com <antazevedo2003@gmail.com>
2024-02-21 17:59:14 +0000

    first commit

---
+++ sheet1.py
@@ -0,0 +1,8 @@
+# 1.1 - two buckets problem
+
+# a) 
+
+# State representation: [W1/W2] -> W1:0..4, W2:0..3
+# Initial state: [0/0]
+# Objective state: [2/_] or in general case [NL/_]
+# Operators: fill a bucket completely - bucket cant be full - fills the bucket to its max capacity - 
