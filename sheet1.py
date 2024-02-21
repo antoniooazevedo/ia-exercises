355180d1fd504a4b1cbde92d695f2d302460a5b0
antazevedo2003@gmail.com <antazevedo2003@gmail.com>
2024-02-21 19:05:32 +0000

    continued (slightly) exercise 1

--- sheet1.py
+++ sheet1.py
@@ -5,4 +5,14 @@
 # State representation: [W1/W2] -> W1:0..4, W2:0..3
 # Initial state: [0/0]
 # Objective state: [2/_] or in general case [NL/_]
-# Operators: fill a bucket completely - bucket cant be full - fills the bucket to its max capacity - 
+# Operators: fill a bucket completely - bucket cant be full - fills the bucket to its max capacity - cost 1
+#            empty a bucket - bucket cant be empty -  empties the bucket to contain no water -  cost 1
+#            pour bucket into another one - goes until either the second one is full or the first one is empty - transfers water between containers - cost 1
+# Objective Test: check that bucket A contains exactly two liters
+
+
+# b)
+
+# Initial state: buckets A and B both empty
+# DUVIDA
+
