f7bb3238fd17e2d444da6e223e9750cf7b04e19d
António Azevedo <antazevedo2003@gmail.com>
2024-03-09 19:45:36 +0000

    solved exercise 1.1 for bucket search

--- sheet1.py
+++ sheet1.py
@@ -14,5 +14,8 @@
 # b)
 
 # Initial state: buckets A and B both empty
-# DUVIDA
-
+# Possible states: 0/0 1/0 2/0 3/0 4/0
+#                  0/1 1/1 2/1 3/1 4/1
+#                  0/2 1/2 2/2 3/2 4/2
+#                  0/3 1/3 2/3 3/3 4/3
+# 20 states (5*4), 4 goal states
